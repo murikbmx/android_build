50229ce82a9f90b97db3bf044c907a72b9206705
murikbmx <murikbmx@gmail.com>
2026-07-04 13:15:20 +0500

    Revert "remove messaging"
    This reverts commit d850c77578a6e8648022a00c3240ab565cf3f7a4.

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -32,6 +32,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # More AOSP packages
 PRODUCT_PACKAGES += \
     initial-package-stopped-states-aosp.xml \
+    messaging \
     PhotoTable \
     preinstalled-packages-platform-aosp-product.xml \
     ThemePicker \
